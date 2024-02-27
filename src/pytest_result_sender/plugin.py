e52f00c8a996072428709a7a79338382a082ce4e
Hertz <1553398021@qq.com>
2024-02-27 16:16:49 +0800

    add unconfigure

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -5,3 +5,9 @@ def pytest_configure():
     # 配置加载完毕后，测试用例执行前执行
 
     print(f"{datetime.now()} pytest_configure")
+
+
+def pytest_unconfigure():
+    #配置卸载完毕之后执行，所有测试用例执行后执行
+
+    print(f"{datetime.now()} pytest_unconfigure")
